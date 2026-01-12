ec2e25717bc3b52263268e91619d2b954922b55a
eugenio martinez <emartinez714@gmail.com>
2026-01-12 13:39:53 -0400

    fix column and Fix the code, it should print the factorial of the argument

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -3,9 +3,10 @@ import sys
 
 def factorial(n):
 	result = 1
-	while n > 1;
+	while n > 1:
 		result *= n
 		n -= 1
-		return result
-	f =factorial (int(sys.argv[1]))
+	return result
+
+	f = factorial(int(sys.argv[1]))
 	print(f)
